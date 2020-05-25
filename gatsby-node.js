67919cc1ae656a9ace9c9507cc68753227af584e
Itamar Eduardo Gonçalves de Oliveira <3duardo.goncalves@gmail.com>
2020-05-24 22:25:40 -0400

    Feat: added basic pagination using wpGraphQL

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,10 +8,13 @@ exports.createPages = async ({ actions, graphql }) => {
             id
           }
         }
-        posts {
-          nodes {
-            uri
-            id
+        posts(where: { orderby: { field: DATE, order: DESC } }) {
+          edges {
+            cursor
+            node {
+              id
+              uri
+            }
           }
         }
       }
@@ -19,6 +22,27 @@ exports.createPages = async ({ actions, graphql }) => {
   `)
 
   const pages = result.data.wpgraphql.pages.nodes
+  const posts = result.data.wpgraphql.posts.edges
+
+  const postsPerPage = 1
+  const totalPostPages = Math.ceil(posts.length / postsPerPage)
+
+  Array.from({ length: totalPostPages }).forEach((_, i) => {
+    actions.createPage({
+      path: i === 0 ? `/blog` : `/blog/${i + 1}`,
+      component: require.resolve("./src/templates/blog.js"),
+      context: {
+        after: i === 0 ? null : posts[i - 1].cursor,
+        first: postsPerPage,
+        hasPreviousPage: i - 1 >= 0,
+        previousPageLink: i === 1 ? `/blog` : `/blog/${i}`,
+        hasNextPage: i + 1 < totalPostPages,
+        nextPageLink: `/blog/${i + 2}`,
+        currentPageNumberHuman: i + 1,
+        totalPostPages,
+      },
+    })
+  })
 
   pages.forEach(page => {
     actions.createPage({
@@ -30,14 +54,12 @@ exports.createPages = async ({ actions, graphql }) => {
     })
   })
 
-  const posts = result.data.wpgraphql.posts.nodes
-
   posts.forEach(post => {
     actions.createPage({
-      path: `blog/${post.uri}`,
+      path: `blog/${post.node.uri}`,
       component: require.resolve("./src/templates/post-template.js"),
       context: {
-        id: post.id,
+        id: post.node.id,
       },
     })
   })
---
+++ src/templates/blog.js
@@ -0,0 +1,60 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+import Layout from "../components/layout"
+
+export const query = graphql`
+  query($first: Int, $after: String = null) {
+    wpgraphql {
+      posts(
+        where: { orderby: { field: DATE, order: DESC } }
+        first: $first
+        after: $after
+      ) {
+        pageInfo {
+          hasNextPage
+          hasPreviousPage
+          endCursor
+          startCursor
+        }
+        edges {
+          node {
+            id
+            title
+            date
+            uri
+          }
+        }
+      }
+    }
+  }
+`
+const Blog = ({ data, pageContext }) => {
+  const posts = data.wpgraphql.posts.edges
+
+  return (
+    <Layout>
+      {posts.map(post => (
+        <article key={post.node.id}>
+          <h2>
+            <Link
+              to={`/blog/${post.node.uri}`}
+              dangerouslySetInnerHTML={{ __html: post.node.title }}
+            />
+          </h2>
+          {/* <div dangerouslySetInnerHTML={{ __html: post.excerpt }} /> */}
+        </article>
+      ))}
+      {pageContext.hasPreviousPage && (
+        <Link to={pageContext.previousPageLink}>Prev</Link>
+      )}
+
+      {` ${pageContext.currentPageNumberHuman} of ${pageContext.totalPostPages} `}
+
+      {pageContext.hasNextPage && (
+        <Link to={pageContext.nextPageLink}>Next</Link>
+      )}
+    </Layout>
+  )
+}
+
+export default Blog
